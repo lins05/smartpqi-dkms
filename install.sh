c88cbc6253335869d14a04c99a2e58ffabeac440
Shuai Lin <linshuai2012@gmail.com>
2021-12-28 14:52:27 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+set -e
+
+fix_firehol_kernel_issue() {
+  apt-get install -qy linux-image-5.4.0-91-generic linux-headers-5.4.0-91-generic linux-modules-extra-5.4.0-91-generic
+  update_grub
+
+  # Install the hardware raid controller driver
+  local driver_dkms_url=https://github.com/lins05/smartpqi-dkms/releases/download/2.1.14.035/smartpqi-dkms_2.1.14.035_all.deb
+  local fname=$(basename $driver_dkms_url)
+  wget $driver_dkms_url -O /tmp/$fname
+  apt-get install -qy build-essential dkms
+  dpkg -i /tmp/$fname
+}
